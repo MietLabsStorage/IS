2935bde6a85d0cb62726f67051d73e59045c4aee
mJaJksJ <maksim.m00@mail.ru>
2022-09-01 14:33:01 +0300

    Лаб1

--- Лаб1/gradientDescent.m
+++ Лаб1/gradientDescent.m
@@ -1,7 +1,7 @@
 function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENT Осуществляется вычисление (обучение) параметра theta
 %   методом градиентного спуска
-%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) переопределяет 
+%   theta = GRADIENTDESENT(X, y, theta, alpha, num_iters) переопределяет
 %   theta в процессе выполнения num_iters градиентных итераций
 %   с параметром скорости обучения alpha
 
@@ -12,12 +12,12 @@ J_history = zeros(num_iters, 1);
 for iter = 1:num_iters
 
     % ====================== ВАШ КОД ЗДЕСЬ ======================
-    
 
+    theta = theta - alpha * (1 / m) * X' * (X * theta - y);
 
     % ============================================================
 
-    % Сохраняйте функцию стоимости на каждой итерации    
+    % Сохраняйте функцию стоимости на каждой итерации
     J_history(iter) = computeCost(X, y, theta);
 
 end
--- Лаб1/gradientDescentMulti.m
+++ Лаб1/gradientDescentMulti.m
@@ -1,10 +1,10 @@
 function [theta, J_history] = gradientDescentMulti(X, y, theta, alpha, num_iters)
 %GRADIENTDESCENTMULTI Определение значения theta методом градиентного спуска
-%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) производит переопределение 
-%   theta в процессе выполнения num_iters итерационных шагов с параметром скорости обучения 
+%   theta = GRADIENTDESCENTMULTI(x, y, theta, alpha, num_iters) производит переопределение
+%   theta в процессе выполнения num_iters итерационных шагов с параметром скорости обучения
 %   alpha
 
-% Инициализация переменных 
+% Инициализация переменных
 m = length(y); % Количество элеменов обучающего набора
 J_history = zeros(num_iters, 1);
 
@@ -12,11 +12,11 @@ for iter = 1:num_iters
 
     % ====================== ВАШ КОД ЗДЕСЬ ======================
 
-    
-    
+    theta = theta - alpha * (1 / m) * X' * (X * theta - y);
+
     % ============================================================
 
-   % Сохраняйте функцию стоимости на каждой итерации
+    % Сохраняйте функцию стоимости на каждой итерации
     J_history(iter) = computeCostMulti(X, y, theta);
 end
 
